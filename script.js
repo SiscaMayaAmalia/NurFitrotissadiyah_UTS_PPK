764106c5c65937f8ceb9414e4c5c84e8f9e0c268
SiscaMayaAmalia <143371667+SiscaMayaAmalia@users.noreply.github.com>
2023-10-30 12:29:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <!-- Your HTML head content goes here -->
-</head>
-<body>
-    <form id="contactForm">
-        <input type="text" id="nama" name="nama" placeholder="Nama">
-        <input type="email" id="email" name="email" placeholder="Email">
-        <textarea id="pesan" name="pesan" placeholder="Pesan"></textarea>
-        <button type="submit">Submit</button>
-    </form>
+document.addEventListener("DOMContentLoaded", function () {
+    const contactForm = document.getElementById('contactForm');
+    
+    contactForm.addEventListener('submit', function (event) {
+        const nama = document.getElementById('nama').value;
+        const email = document.getElementById('email').value;
+        const pesan = document.getElementById('pesan').value;
 
-    <script>
-        document.addEventListener("DOMContentLoaded", function () {
-            const contactForm = document.getElementById('contactForm');
-            
-            contactForm.addEventListener('submit', function (event) {
-                const nama = document.getElementById('nama').value;
-                const email = document.getElementById('email').value;
-                const pesan = document.getElementById('pesan').value;
-
-                if (!nama || !email || !pesan) {
-                    alert('Silakan isi semua bidang yang diperlukan.');
-                    event.preventDefault();
-                }
-            });
-        });
-    </script>
-</body>
-</html>
+        if (!nama || !email || !pesan) {
+            alert('Silakan isi semua bidang yang diperlukan.');
+            event.preventDefault();
+        }
+    });
+});
