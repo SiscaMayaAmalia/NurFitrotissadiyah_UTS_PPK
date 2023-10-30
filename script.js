ceb76d323e147949ff42f61acc3f53e65d9f3733
SiscaMayaAmalia <143371667+SiscaMayaAmalia@users.noreply.github.com>
2023-10-30 12:27:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const contactForm = document.getElementById('contactForm');
-    
-    contactForm.addEventListener('submit', function (event) {
-        const nama = document.getElementById('nama').value;
-        const email = document.getElementById('email').value;
-        const pesan = document.getElementById('pesan').value;
+<script>
+        document.addEventListener("DOMContentLoaded", function () {
+            const contactForm = document.getElementById('contactForm');
+            
+            contactForm.addEventListener('submit', function (event) {
+                const nama = document.getElementById('nama').value;
+                const email = document.getElementById('email').value;
+                const pesan = document.getElementById('pesan').value;
 
-        if (!nama || !email || !pesan) {
-            alert('Silakan isi semua bidang yang diperlukan.');
-            event.preventDefault();
-        }
-    });
-});
+                if (!nama || !email || !pesan) {
+                    alert('Silakan isi semua bidang yang diperlukan.');
+                    event.preventDefault();
+                }
+            });
+        });
+    </script>
